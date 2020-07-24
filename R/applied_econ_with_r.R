3815df037fe2895450986888aebd33a3eb952d8c
Hans H. Sievertsen <hhsievertsen@users.noreply.github.com>
2020-07-24 08:15:33 +0100

    updated data

--- R/applied_econ_with_r.R
+++ R/applied_econ_with_r.R
@@ -1,13 +1,16 @@
 applied_econ_with_r<-function(){
   print("This package contains the following data sets:")
 }
-#library("tidyverse")
-#mpd<-read_csv("mpd2018.csv")%>%
-  #  mutate(gdp=pop*rgdpnapc)%>%
-  #     select(country, year,gdp,pop )
-#dfa<-mpd%>%group_by(year)%>%summarise(gdp=sum(gdp,na.rm = TRUE),pop=sum(pop,na.rm = TRUE))%>%
-  #  filter(gdp!=0)%>%mutate(country="World")
-#mpd<-rbind(mpd,dfa)
-#mpd<-mpd%>%mutate(gdp_per_capita=gdp/pop)%>%rename(region=country)%>%select(-pop,-gdp)%>%filter(!is.na(gdp_per_capita))
+# library("tidyverse")
+# mpd<-read_csv("mpd2018.csv")%>%
+#   mutate(gdp=pop*rgdpnapc)%>%
+#      select(country, year,gdp,pop )%>%
+#   mutate(gdp=ifelse(!is.na(pop),gdp,NA))%>%
+#   mutate(pop=ifelse(!is.na(gdp),pop,NA))
+#
+# dfa<-mpd%>%group_by(year)%>%summarise(gdp=sum(gdp,na.rm = TRUE),pop=sum(pop,na.rm = TRUE))%>%
+#   filter(gdp!=0)%>%mutate(country="World")
+# mpd<-rbind(mpd,dfa)
+# mpd<-mpd%>%mutate(gdp_per_capita=gdp/pop)%>%rename(region=country)%>%select(-pop,-gdp)%>%filter(!is.na(gdp_per_capita))
 
 #save(mpd, file="data/mpd.RData")
