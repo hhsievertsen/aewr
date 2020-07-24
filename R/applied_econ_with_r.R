523343a687dba8b361fc2f17aa4ab8329c9c53cd
Hans H. Sievertsen <hhsievertsen@users.noreply.github.com>
2020-07-24 07:56:38 +0100

    push data

--- R/applied_econ_with_r.R
+++ R/applied_econ_with_r.R
@@ -3,7 +3,11 @@ applied_econ_with_r<-function(){
 }
 #library("tidyverse")
 #mpd<-read_csv("mpd2018.csv")%>%
-#    mutate(gdp=pop*rgdpnapc)%>%
-#     select(country, year,gdp,pop )
+  #  mutate(gdp=pop*rgdpnapc)%>%
+  #     select(country, year,gdp,pop )
+#dfa<-mpd%>%group_by(year)%>%summarise(gdp=sum(gdp,na.rm = TRUE),pop=sum(pop,na.rm = TRUE))%>%
+  #  filter(gdp!=0)%>%mutate(country="World")
+#mpd<-rbind(mpd,dfa)
+#mpd<-mpd%>%mutate(gdp_per_capita=gdp/pop)%>%rename(region=country)%>%select(-pop,-gdp)%>%filter(!is.na(gdp_per_capita))
 
 #save(mpd, file="data/mpd.RData")
