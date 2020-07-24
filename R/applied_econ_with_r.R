24ce728b0d7564bfe0c391220f61a422ae40d551
Hans H. Sievertsen <hhsievertsen@users.noreply.github.com>
2020-07-24 07:39:25 +0100

    added data

--- R/applied_econ_with_r.R
+++ R/applied_econ_with_r.R
@@ -1,3 +1,9 @@
 applied_econ_with_r<-function(){
   print("This package contains the following data sets:")
 }
+#library("tidyverse")
+#mpd<-read_csv("mpd2018.csv")%>%
+#    mutate(gdp=pop*rgdpnapc)%>%
+#     select(country, year,gdp,pop )
+
+#save(mpd, file="data/mpd.RData")
